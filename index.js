c3954577adee7445cf6afd07c4b7ff6a5262cd4a
Alessandro Marin <alessandro.m316@gmail.com>
2018-03-12 16:41:01 -0400

    should correctly generate the files

--- index.js
+++ index.js
@@ -24,7 +24,13 @@ const camelize = function(str) {
     })
 }
 
-const getInTypes = function(args) {
+const getInputs = function(args) {
+    return args
+        .filter(a => a[0].indexOf("in") == 0 || a[0].indexOf("optIn") == 0)
+        .map(a => [camelize(a[0].replace(/^(in|optIn)/, "")), a[1]])
+}
+
+const getInputTypes = function(args) {
     const typeMap = {
         f32: "TA_Real",
         f64: "TA_Real",
@@ -37,7 +43,7 @@ const getInTypes = function(args) {
             let [n, t] = a
 
             if (t.indexOf('*') == 0) {
-                let innerType = typeMap[t.match(/([a-z][0-9]+)/)[1]]
+                let innerType = typeMap[t.match(/([a-z][0-9][0-9])/)[1]]
                 t = `&Vec<${innerType}>`
             } else {
                 t = typeMap[t]
@@ -47,58 +53,105 @@ const getInTypes = function(args) {
         })
 }
 
-const getInputs = function(args) {
-    return args
-        .filter(a => a[0].indexOf("in") == 0 || a[0].indexOf("optIn") == 0)
-        .map(a => [camelize(a[0].replace(/^(in|optIn)/, "")), a[1]])
-}
-
 const getOutputs = function(args) {
     return args
         .filter(a => a[0].indexOf("out") == 0)
+        // remove outs that are common to all functions
+        .filter(a => ["outBegIdx", "outNBElement"].indexOf(a[0]) < 0)
+}
+
+const getOutputTypes = function(args) {
+    const typeMap = {
+        "::std::os::raw::c_int": "TA_Integer",
+        f32: "TA_Real",
+        f64: "TA_Real"
+    }
+
+    return args
+        .map(a => {
+            let [n, t] = a
+
+            let innerType = typeMap[t.match(/\*[a-z]+\s+([\w:*0-9]+)/)[1]]
+            t = `Vec<${innerType}>`
+
+            return [n, t]
+        })
 }
 
 const generateCode = function(fn) {
     let inputs = getInputs(fn.args)
-    inputs = getInTypes(inputs)
-    const output = getOutputs(fn.args)
-    console.log(inputs)
+    inputs = getInputTypes(inputs)
+    let outputs = getOutputs(fn.args)
+    outputs = getOutputTypes(outputs)
+
+    const inputTypes = inputs
+        .map(i => `${i[0]}: ${i[1]}`)
+        .join(', ')
+    const outputTypes = outputs
+        .map(o => o[1])
+        .join(', ')
+
+    // We're under the assumptions that the only output declarations are Vecs
+    // and that the first input is always a Vec from which we can derive the output's len
+    const outputDeclarations = outputs
+        .map(o => `let mut ${o[0]}: ${o[1]} = Vec::with_capacity(${inputs[0].name}.len());`)
+        .join('\n')
+    const outputArgs = outputs
+        .map(o => `${o[0]}.as_mut_ptr()                // pointer to the first element of the output vector`)
+        .join(',\n')
+
+    const inputArgs = inputs
+        .map(i => {
+            if (i[1].indexOf("Vec<") > 0)
+                return `${i[0]}.as_ptr()                  // pointer to the first ${i[0]} of the vector`
+            else
+                return `${i[0]}`
+        })
+        .join(',\n')
+
+
+    const onSuccessSetSizes = outputs
+        .map(o => `${o[0]}.set_len(out_size as usize);`)
+        .join('\n')
+
+    const returnOutputs = outputs
+        .map(o => o[0])
+        .join(', ')
+
     return `
-use ta_lib_wrapper::{TA_Integer, TA_Real, TA_${fn.name},  TA_RetCode};
+use ta_lib_wrapper::{TA_Integer, TA_Real, TA_${fn.name}, TA_RetCode, TA_MAType};
 
-
-pub fn tristar(open: &Vec<TA_Real>, high: &Vec<TA_Real>, low: &Vec<TA_Real>, close: &Vec<TA_Real>) -> (Vec<TA_Integer>, TA_Integer) {
-    let mut out: Vec<TA_Integer> = Vec::with_capacity(open.len());
+pub fn tristar(${inputTypes}) -> (${outputTypes}, TA_Integer) {
+    ${outputDeclarations}
     let mut out_begin: TA_Integer = 0;
     let mut out_size: TA_Integer = 0;
 
     unsafe {
-        let ret_code = TA_CDLTRISTAR(
-            0,                              // index of the first close to use
-            open.len() as i32 - 1,          // index of the last close to use
-            open.as_ptr(),                  // pointer to the first element of the vector
-            high.as_ptr(),                  // pointer to the first element of the vector
-            low.as_ptr(),                   // pointer to the first element of the vector
-            close.as_ptr(),                 // pointer to the first element of the vector
+        let ret_code = TA_${fn.name}(
+            0,                              // index of the first element to use
+            ${inputs[0].name}.len() as i32 - 1,          // index of the last element to use
+            ${inputArgs}
             &mut out_begin,                 // set to index of the first close to have an rsi value
             &mut out_size,                  // set to number of sma values computed
-            out.as_mut_ptr()                // pointer to the first element of the output vector
+            ${outputArgs}
         );
         match ret_code {
             // Indicator was computed correctly, since the vector was filled by TA-lib C library,
             // Rust doesn't know what is the new length of the vector, so we set it manually
             // to the number of values returned by the TA_CDLTRISTAR call
-            TA_RetCode::TA_SUCCESS => out.set_len(out_size as usize),
+            TA_RetCode::TA_SUCCESS => {
+                ${onSuccessSetSizes}
+            },
             // An error occured
             _ => panic!("Could not compute indicator, err: {:?}", ret_code)
         }
     }
 
-    (out, out_begin)
+    (${returnOutputs}, out_begin)
 }
     `
 }
 
 
-fns.map(f => generateCode(f))
+console.log(fns.map(f => generateCode(f)))
 // console.log(fns[0].args)
